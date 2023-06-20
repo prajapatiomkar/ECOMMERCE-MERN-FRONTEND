dc1b0383cca26ac666f3b0d4ea163eef9fe07208
prajapati omkar <prajapatiomkar9833@gmail.com>
2023-06-20 10:23:02 +0530

    Project Initialized Successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ProductList from "./features/productList/ProductList";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from '../features/navbar/Navbar'
+import ProductList from '../features/productList/ProductList'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar>
+            <ProductList></ProductList>
+        </Navbar>
+    </div>
+  )
+}
