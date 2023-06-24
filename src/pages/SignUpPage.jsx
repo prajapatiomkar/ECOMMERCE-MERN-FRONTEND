69e8704a9f5001cc05b1b981369c1ddfaaa272e0
prajapati omkar <prajapatiomkar9833@gmail.com>
2023-06-24 11:20:34 +0530

    Checkout Page/Order Completed

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import SignUp from "../features/auth/components/SignUp";
+import SignUp from "../features/auth/components/Signup";
 
 export default function SignupPage() {
   return <SignUp />;
